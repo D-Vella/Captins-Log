4320b90b6e986458778fc14366e57e59d1b29426
David Vella <david@vellanet.co.uk>
2026-08-23 17:21:19 +0100

    Refinements after review.

--- services/discogs.py
+++ services/discogs.py
@@ -1,3 +1,4 @@
+import re
 import discogs_client
 import pandas as pd
 
@@ -9,17 +10,21 @@ def get_discogs_collection() -> pd.DataFrame:
 
     df = pd.DataFrame({'Artist': pd.Series(dtype='str'),
                        'Title': pd.Series(dtype='str'),
-                       'ReleaseYear': pd.Series(dtype='int'),
+                       'PressingYear': pd.Series(dtype='int'),
                        'DateAdded': pd.Series(dtype='datetime64[ns]'),
                        'Format': pd.Series(dtype='str'),
-                       'ReleaseID': pd.Series(dtype='int')})
+                       'ReleaseID': pd.Series(dtype='int'),
+                       'ReleaseMasterID': pd.Series(dtype='int')})
 
     for item in me.collection_folders[0].releases:
-        df.loc[len(df)] = {'Artist': item.release.artists[0].name,
-                           'Title': item.release.title,
-                           'ReleaseYear': item.release.year,
-                           'DateAdded': str(item.date_added)[0:10],  # Just want the date.
-                           'Format': item.release.formats[0]['name'],
-                           'ReleaseID': item.release.id}
+        df.loc[len(df)] = {'Artist': re.sub(r'\s*\(\d+\)$', '', item.release.artists[0].name),
+                        'Title': item.release.title,
+                        'PressingYear': item.release.year if item.release.year != 0 else None,
+                        'DateAdded': str(item.date_added)[0:10], #Just want the date.
+                        'Format': item.release.formats[0]['name'],
+                        'ReleaseID': item.release.id,
+                        'ReleaseMasterID': item.release.master.id if item.release.master else None,
+    }
 
+    df = df.sort_values(by=['DateAdded'], ascending=False).reset_index(drop=True)
     return df
